fd35d83ef2a9bf3136f1cca1430efc8a3c4ad2f6
xuefei <xuefeiwang4@creditease.cn>
2016-04-22 15:33:11 +0800

    2016/4/22 update

---
+++ Struts2/src/main/java/cn/rookie/action/HelloAction.java
@@ -0,0 +1,17 @@
+package cn.rookie.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by Rookie on 2016/4/22.
+ * Description:
+ * Project_name: Framework-base
+ * Copyright (c) All Rights Reserved.
+ */
+public class HelloAction extends ActionSupport {
+
+    public String execute() throws Exception {
+
+        return SUCCESS;
+    }
+}
---
+++ Struts2/src/main/webapp/login.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Congurations ！</h2>
+</body>
+</html>
