d60584886cc4fd4e11436147b2da9f679d8aa974
xuefei <xuefeiwang4@creditease.cn>
2016-04-25 18:50:41 +0800

    2016/4/25  spring

---
+++ Spring-base/src/main/java/cn/rookie/post/MyBeanPostProcessor.java
@@ -0,0 +1,38 @@
+package cn.rookie.post;
+
+import cn.rookie.post.bean.Chinese;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * Created by Rookie on 2016/4/25.
+ * Description: Bean后处理器
+ * Project_name: Framework-base
+ * Copyright (c) All Rights Reserved.
+ */
+public class MyBeanPostProcessor implements BeanPostProcessor{
+
+    /**
+     * 对容器中的Bean实例进行后处理
+     * @param bean 需要进行后处理的原Bean实例
+     * @param beanName 需要进行后处理的Bean类的配置id
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Bean后处理器在初始化之前对" + beanName + "进行增强型处理");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Bean后处理器在初始化之后对" + beanName + "进行增强型处理");
+
+        if (bean instanceof Chinese) {
+            Chinese chinese = (Chinese) bean;
+            chinese.setName("后处理器更改了名称");
+        }
+        return bean;
+    }
+}
---
+++ Spring-base/src/test/java/cn/rookie/testPost.java
@@ -0,0 +1,28 @@
+package cn.rookie;
+
+import cn.rookie.event.EmailEvent;
+import cn.rookie.post.bean.Chinese;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Rookie on 2016/4/25.
+ * Description:
+ * Project_name: Framework-base
+ * Copyright (c) All Rights Reserved.
+ */
+public class TestPost {
+    private ApplicationContext ctx;
+    @Before
+    public void setUp() {
+        ctx = new ClassPathXmlApplicationContext("beans.xml");
+    }
+    @Test
+    public void testrPost() {
+        Chinese c = (Chinese) ctx.getBean("chinesePost");
+        c.useAxe();
+    }
+
+}
